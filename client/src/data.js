245c3b683d59a2fcd0674a400ce7fa8eef431529
Nakul <nakulgarg1424@gmail.com>
2024-06-10 02:01:05 +0530

    datafile

---
+++ client/src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: '/shopping1.png',
+    title: 'SUMMER SALE',
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: '#f5fafd',
+  },
+  {
+    id: 2,
+    img: '/shopping2.png',
+    title: 'AUTUMN COLLECTION',
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: '#fcf1ed',
+  },
+  {
+    id: 3,
+    img: '/shopping3.png',
+    title: 'LOUNGEWEAR LOVE',
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: '#fbf0f4',
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: '/lounge.jpg',
+    title: 'LOUNGEWARE LOVE',
+  },
+  {
+    id: 2,
+    img: '/shirt.jpg',
+    title: 'SHIRT STYLE!',
+  },
+  {
+    id: 3,
+    img: '/jacket.webp',
+    title: 'LIGHT JACKETS',
+  },
+];
+
+export const popularProducts = [
+  {
+    _id: 1,
+    img: '/bag.png',
+  },
+  {
+    _id: 2,
+    img: '/jack.png',
+  },
+  {
+    _id: 3,
+    img: '/kimono.png',
+  },
+  {
+    _id: 4,
+    img: '/sunglass.png',
+  },
+  {
+    _id: 5,
+    img: '/sweater.png',
+  },
+  {
+    _id: 6,
+    img: '/trouser.png',
+  },
+  {
+    _id: 7,
+    img: '/tshirt.png',
+  },
+  {
+    _id: 8,
+    img: '/watch.png',
+  },
+];
