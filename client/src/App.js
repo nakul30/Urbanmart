047e50d1c08b799b2570117c0a17ef8f41f9c099
Nakul <nakulgarg1424@gmail.com>
2024-06-21 15:22:41 +0530

    Home.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,27 @@
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Succcess from "./pages/Success";
 import { BrowserRouter, Routes, Route ,Navigate} from "react-router-dom";
 import { useSelector } from "react-redux";
+import Register from "./pages/Register";
+import Address from "./pages/Address";
+import Cart from "./pages/Cart";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
 function App() {
   const user = useSelector((state) => state.user);
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<Product/>} />
+        <Route path="/products/:category" element={<ProductList />} />
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/cart" element={<Cart />} />
         <Route path="/login" element={user.current?<Navigate to="/"/>:<Login />}/>
-        
+        <Route path="/register" element={user.current ? <Navigate to="/" /> : <Register />} />
+        <Route path="/success" element={<Succcess />} />
+        <Route path="/address" element={<Address />} />
       </Routes>
     </BrowserRouter>
   );
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,20 +1,25 @@
 import React from 'react'
+import Announcement from '../components/Announcement'
 import Category from '../components/Category'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
+import Newsletter from '../components/Newsletter'
 import Products from '../components/Products'
-
+import Slider from '../components/Slider';
 function Home() {
   return (
     <>
-    <div>Home</div>
+    {/* <div>Home</div> */}
+    <Announcement/>
     <Navbar/>
+    <Slider/>
     <Category/>
-    <Products/>
+    {/* <Products/> */}
+    <Newsletter/>
     <Footer/>
     </>
     
   )
 }
 
-export default Home
+export default Home;
