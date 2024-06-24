184572c9ec1e2092d847d8154f547b127cd5c463
Nakul <nakulgarg1424@gmail.com>
2024-06-24 22:12:46 +0530

    backend route prblm

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,9 +7,17 @@ const productRouter = require('./product');
 const cartRouter = require('./cart');
 const orderRouter = require('./order');
 const razorRouter = require('./razor');
+const verifyToken = require("../middlewares/verifyToken");
 
 console.log("Router deployed");
-
+router.get('/test', verifyToken , async(req,res)=>{
+    try{
+        res.status(200).json("Welcome to the backend");
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+})
 router.use('/auth', authRouter);
 router.use('/user', userRouter);
 router.use('/product', productRouter);
